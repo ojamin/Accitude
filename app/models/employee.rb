a7a2a819b0f916ef2ce51eecc8b62d0c9573e858
Benjamin Ohana <ben@ebotech.com>
2010-03-01 17:13:41 +0000

    disabled NI requirement

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -8,6 +8,6 @@ class Employee < ActiveRecord::Base
   has_many :expenses
   
   validates_presence_of :name, :street, :address, :postcode, :phone
-  validates_format_of :ni_number, :with => /^([a-z]|[A-Z]){2}(\d){6}([a-z]|[A-Z]){0,1}$/
+#  validates_format_of :ni_number, :with => /^([a-z]|[A-Z]){2}(\d){6}([a-z]|[A-Z]){0,1}$/
 
 end
