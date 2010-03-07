a88b31d690d445463038ec6a43922fe40c5afd6d
Matthew McCorry <corey@corey-desktop.(none)>
2010-03-07 22:10:48 +0900

    Date filtering added to invoices.  Still needs more work, but functions

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -17,6 +17,10 @@ class InvoicesController < ApplicationController
       @invoices = @current_org.invoices.find_all_by_processed(true).paginate :page => (params[:page] || '1')
     elsif params[:filter] == 'Unprocessed'
       @invoices = @current_org.invoices.find_all_by_processed(false).paginate :page => (params[:page] || '1')
+    elsif params[:filter] == 'Before'
+      @invoices = @current_org.invoices.find(:all, :conditions => ["produced_on < ?", Time.parse(params[:date])]).paginate :page => (params[:page] || '1')
+    elsif params[:filter] == 'After'
+      @invoices = @current_org.invoices.find(:all, :conditions => ["produced_on > ?", Time.parse(params[:date])]).paginate :page => (params[:page] || '1')
     else
       @invoices = @current_org.invoices.paginate :page => (params[:page] || '1')
     end
