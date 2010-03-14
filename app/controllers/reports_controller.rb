971111d269549efb59d82681007e4e3a33d2a643
Benjamin Ohana <ben@ebotech.com>
2010-03-14 22:34:49 +0000

    Unpaids report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,2 +1,9 @@
 class ReportsController < ApplicationController
+
+  def unpaids
+    invoices = @current_org.invoices.find_all_by_paid_on nil
+    liabilities = @current_org.liabilities.find_all_by_paid_on nil
+    ren_cont 'unpaids', {:invoices => invoices, :liabilities => liabilities}
+  end
+
 end
